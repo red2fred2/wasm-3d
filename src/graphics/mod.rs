187e92ba96e9b042acb9a4168e5fdb87d6cebc8d
red2fred2 <red2fred2base@live.com>
2022-02-14 18:12:23 -0600

    Created Graphics struct This is to make it easier to call render externally, so the caller doesn't have to handle contexts and shaders

--- src/graphics/mod.rs
+++ src/graphics/mod.rs
@@ -5,63 +5,65 @@ use std::collections::HashMap;
 use web_sys::{WebGlRenderingContext, WebGlProgram};
 use self::shaders::shader_sources::get_shader_sources;
 
-/// Initialize graphics
-pub fn init() {
-	// Set up the front end
-	let context = gl::set_up_canvas();
-
-	// Compile shaders
-	let shader_source = get_shader_sources();
-	let mut shaders = HashMap::new();
-	for (name, source) in shader_source.iter() {
-		let compiled_shader = gl::build_program(&context, source);
-		shaders.insert(name as &str, compiled_shader);
-	}
-
-	// Render a frame
-	render(&context, &shaders);
+pub struct Graphics {
+	webgl: WebGlRenderingContext,
+	shaders: HashMap<&'static str, Option<WebGlProgram>>
 }
 
-/// Renders a frame to the screen
-///
-/// * `context` - webGL rendering context
-/// * `screen_width` - the width of the screen in pixels
-/// * `screen_height` - the height of the screen in pixels
-/// * `shaders` - the compiled shaders available to use
-fn render(
-	context: &web_sys::WebGlRenderingContext,
-	shaders: &HashMap<&str, Option<WebGlProgram>>
-) {
-	// Set shader
-	let shader = shaders.get("Basic bitch").unwrap();
-    context.use_program(shader.as_ref());
+impl Graphics {
 
-	// Initialize the array buffer
-	let array_buffer = context.create_buffer();
-	context.bind_buffer(WebGlRenderingContext::ARRAY_BUFFER, array_buffer.as_ref());
+	/// Initialize graphics
+	pub fn init() -> Graphics {
+		// Set up the front end
+		let context = gl::set_up_canvas();
 
-	// Set up the shared memory buffer between rust and the web
-	unsafe {
-		context.buffer_data_with_array_buffer_view(
-			WebGlRenderingContext::ARRAY_BUFFER,
-			&js_sys::Float32Array::view(&vec![
-				1.0, 1.0, 0.0,
-				0.0, 1.0, 0.0,
-				0.0, 0.0, 0.0,
+		// Compile shaders
+		let shader_source = get_shader_sources();
+		let mut shaders = HashMap::new();
 
-				0.0, 0.0, 0.0,
-				1.0, 1.0, 0.0,
-				1.0, 0.0, 0.0
-			]),
-			WebGlRenderingContext::STATIC_DRAW,
-		);
+		for (name, source) in shader_source.iter() {
+			let compiled_shader = gl::build_program(&context, source);
+			shaders.insert(name as &str, compiled_shader);
+		}
+
+		// Return newly created Graphics object
+		Graphics { webgl: context, shaders: shaders }
 	}
 
-    context.vertex_attrib_pointer_with_i32(0, 3, WebGlRenderingContext::FLOAT, false, 0, 0);
-	context.enable_vertex_attrib_array(0);
+	/// Renders a frame to the screen
+	pub fn render(&self) {
+		// Set shader
+		let shader = self.shaders.get("Basic bitch").unwrap();
+		self.webgl.use_program(shader.as_ref());
 
-	// Clear the screen then draw
-	context.clear_color(0.0, 0.0, 0.0, 1.0);
-	context.clear(WebGlRenderingContext::COLOR_BUFFER_BIT);
-	context.draw_arrays(WebGlRenderingContext::TRIANGLES, 0, 6);
+		// Initialize the array buffer
+		let array_buffer = self.webgl.create_buffer();
+		self.webgl.bind_buffer(WebGlRenderingContext::ARRAY_BUFFER, array_buffer.as_ref());
+
+		// Set up the shared memory buffer between rust and the web
+		unsafe {
+			self.webgl.buffer_data_with_array_buffer_view(
+				WebGlRenderingContext::ARRAY_BUFFER,
+				&js_sys::Float32Array::view(&vec![
+					1.0, 1.0, 0.0,
+					0.0, 1.0, 0.0,
+					0.0, 0.0, 0.0,
+
+					0.0, 0.0, 0.0,
+					1.0, 1.0, 0.0,
+					1.0, 0.0, 0.0
+				]),
+				WebGlRenderingContext::STATIC_DRAW,
+			);
+		}
+
+		self.webgl.vertex_attrib_pointer_with_i32(0, 3, WebGlRenderingContext::FLOAT, false, 0, 0);
+		self.webgl.enable_vertex_attrib_array(0);
+
+		// Clear the screen then draw
+		self.webgl.clear_color(0.0, 0.0, 0.0, 1.0);
+		self.webgl.clear(WebGlRenderingContext::COLOR_BUFFER_BIT);
+		self.webgl.draw_arrays(WebGlRenderingContext::TRIANGLES, 0, 6);
+	}
+
 }
--- src/lib.rs
+++ src/lib.rs
@@ -7,5 +7,6 @@ pub mod graphics;
 /// Function called from the browser to initialize the rust program
 #[wasm_bindgen]
 pub fn init_rs() {
-	crate::graphics::init();
+	let graphics = crate::graphics::Graphics::init();
+	graphics.render();
 }
